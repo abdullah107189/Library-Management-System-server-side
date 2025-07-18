2153570c06b6f28c294c8745d8be2ba74bca29f3
abdullah107189 <abdullah107189@gmail.com>
2025-07-18 23:23:56 +0600

    set up shadcn, tailwind, react ts and react router dom

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul className="flex gap-3 border ">
+        <li>Home</li>
+        <li>Book</li>
+        <li>borrow</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
